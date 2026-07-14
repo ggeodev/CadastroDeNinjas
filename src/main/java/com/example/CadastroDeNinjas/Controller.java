e4d5e1b5675e754d9cb3a363587c9b993fdd9766
geovane <geovanecosmo93@.gmail.com>
2026-07-14 02:29:36 -0300

    Criando controller

---
+++ src/main/java/com/example/CadastroDeNinjas/Controller.java
@@ -0,0 +1,19 @@
+package com.example.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Minha primeira mensagem nessa rota";
+    }
+
+
+
+
+}
